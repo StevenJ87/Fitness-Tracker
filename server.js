34f473f42ae8550615c4253ed4b2af394f87d905
Steven Johnson <sjohnson21960@gmail.com>
2020-08-07 10:31:08 -0500

    heroku deployment ready

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workoutDB"
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
